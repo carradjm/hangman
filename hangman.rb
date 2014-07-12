a1b713643c96d26881c456e37e661dcdcd37bb98
Malcolm Carradine <malcolm.carradine@gmail.com>
2014-07-12 16:41:55 -0400

    Started adding stuff to Hangman

--- hangman.rb
+++ hangman.rb
@@ -1,4 +1,82 @@
 class Hangman
 
+  def initialize(guessing_player, checking_player)
+    @guessing_player = guessing_player
+    @checking_player = checking_player
+  end
 
+  def play
+    show_blank_word
+    while show_word.any
+  end
+
+  def show_word
+    board = []
+
+    @guessing_player.length.times do
+      board << "_"
+    end
+
+    board
+  end
+end
+
+class HumanPlayer
+
+  attr_reader :secret_word, :length
+
+  def initialize
+    @secret_word = ''
+  end
+
+  def pick_secret_word
+  end
+
+  def receive_secret_length
+  end
+
+  def guess
+    puts "Guess a letter: "
+    letter = gets.chomp
+  end
+
+  def check_guess
+  end
+
+  def handle_guess_response
+  end
+
+end
+
+class ComputerPlayer
+
+  attr_reader :secret_word, :length
+
+  def initialize
+    @secret_word = ''
+    @length = 0
+  end
+
+  def pick_secret_word
+    dictionary = File.readlines('dictionary.txt').chomp
+    @secret_word = dictionary.sample
+  end
+
+  def receive_secret_length
+    @length = @secret_word.length
+  end
+
+  def guess
+  end
+
+  def check_guess
+  end
+
+  def handle_guess_response
+  end
+
+end
+
+if __FILE__ == $PROGRAM_NAME
+  Hangman.new(HumanPlayer.new, ComputerPlayer.new)
 end
